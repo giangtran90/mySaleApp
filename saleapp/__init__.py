053adeed20286c3b2d6390d19245766b88df6761
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-09-12 18:31:16 +0900

    tao bang trong CSDL mySQL bang ORM

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 """
     xac dinh bien app se dung nhieu cho khac nhau nen dat ben trong init(file khoi dong package)
     name: bien toan cuc truyen vao
 """
 app = Flask(__name__)
+"""
+cau hinh thong so ket noi
+"""
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/labsaledb?charset=utf8mb4'
+
+"""
+Can cau hinh ket noi
+"""
+db = SQLAlchemy(app=app)
